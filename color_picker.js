cddb7cc4104c202151e3feaa4f822ac821619105
MortCodesWeb <52796888+mortada-killer0@users.noreply.github.com>
2022-04-26 19:26:39 +0300

    Add files via upload

--- color_picker.js
+++ color_picker.js
@@ -4,23 +4,23 @@ function colorPicker(url, _config = {}) {
   var canvas = document.createElement('canvas');
 
   // XXX: We have to temproraily save the colors in a value since
-  // we can't return them straightaway from useCanvas()'s callback.
-  var color = [];
+  //      we can't return them straightaway from useCanvas()'s callback.
+  var colors = [];
 
   useCanvas(canvas, url, function() {
-      for (var i = 0; i <= (_config.colors || 1); i++) {
+      for (var i = 0; i <= (_config.colors - 1 || 0); i++) {
         // XXX: Here we get the image's data from each seperated part.
         var p = canvas.getContext('2d').getImageData(i, i, (_config.colors || 1), (_config.colors || 1)).data;
 
         // XXX: Here we multiply the color value with the brightness you
-        // specified to change it's brightness.
+        //      specified to change it's brightness.
         var r = p[0] * (_config.brightness || 1);
         var g = p[1] * (_config.brightness || 1);
         var b = p[2] * (_config.brightness || 1);
 
         // XXX: And we get the color values and turn them to rgba CSS values
-        // with optional opacity.
-        color[i] = 'rgba(' + r + ', ' + g + ', ' + b + ', ' + (_config.opacity || 1) + ')';
+        //      with optional opacity.
+        colors[i] = 'rgba(' + r + ', ' + g + ', ' + b + ', ' + (_config.opacity || 1) + ')';
       }
     });
 
@@ -39,5 +39,9 @@ function colorPicker(url, _config = {}) {
   }
 
   // XXX: And there you get the array of the colors in the image.
-  return color;
+  if (_config.linearGradient !== undefined) {
+    return 'linear-gradient(' + _config.linearGradient + ', ' + colors + ')';
+  } else {
+    return colors;
+  }
 }
